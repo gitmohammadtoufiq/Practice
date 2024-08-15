3c9fa3f6ad834593512e3a074d8693857450483e
mohammad <maroof.raktim@gmail.com>
2024-08-15 17:06:24 -0400

    first commit

--- src/Prac/Hello.java
+++ src/Prac/Hello.java
@@ -3,7 +3,7 @@ package Prac;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello worldaghlagjlsjglajglajgla");
 
 	}
 
